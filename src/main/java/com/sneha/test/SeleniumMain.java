9183fe00b5b56743e3ef18d98e8a0dc49f8c7763
Navneeth <navneethraj456@gmail.com>
2021-03-21 17:24:59 +0000

    Deleting un-necessaru file

--- src/main/java/com/sneha/test/SeleniumMain.java
+++
@@ -1,7 +0,0 @@
-public class SeleniumMain {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        String url = " https://www.tutorialspoint.com/questions/index.php";
-        driver.get(url);
-    }
-}
